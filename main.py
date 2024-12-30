1aee852023018476fb9557b62ce4768c0cf81f2b
RémyMD <remymarquentin@gmail.com>
2024-12-30 10:39:32 +0100

    Add comment

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
+#import de pandas qui permet de lire le fichier .txt SMSSpamCollection.txt'
 import pandas as pd 
 from fastapi import FastAPI
 
+#Initialise pandas pour qu'il puisse convertir le fichier en liste utilisable avec label et message en clé
 df = pd.read_csv('data/SMSSpamCollection.txt', sep='\t', header=None, names=['label', 'message'])
-df['label'] = df['label'].map({'ham': 0, 'spam': 1})
 
 app = FastAPI()
 
+#Fonction qui permet de détecté les spam j'ai mis une liste de mot assez bateau
 def classify_message(message):
     spam_keywords = ['free', 'win', 'cash', 'prize', 'call now', 'urgent']
     for word in spam_keywords:
@@ -13,6 +15,7 @@ def classify_message(message):
             return "spam"
     return "ham"
 
+#Permet de tester les messages
 @app.get("/check")
 def check_message(message: str):
     result = classify_message(message)
