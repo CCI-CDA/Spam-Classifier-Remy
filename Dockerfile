b991195888c932cca087b9b5d25c09f9baf84d6c
Rémy <remymarquentin@gmail.com>
2025-01-16 14:18:20 +0100

    HTML page

--- Dockerfile
+++ Dockerfile
@@ -4,7 +4,6 @@ WORKDIR /app
 
 COPY requirements.txt ./
 RUN pip install --no-cache-dir -r requirements.txt
-RUN pip install uvicorn
 RUN pip install fastapi[standard]
 COPY . .
 
--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 #import de pandas qui permet de lire le fichier .txt SMSSpamCollection.txt'
 import pandas as pd 
-from fastapi import FastAPI
+from fastapi import FastAPI,Request
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import HTMLResponse, RedirectResponse
+from fastapi.templating import Jinja2Templates
 
 #Initialise pandas pour qu'il puisse convertir le fichier en liste utilisable avec label et message en clé
 df = pd.read_csv('data/SMSSpamCollection.txt', sep='\t', header=None, names=['label', 'message'])
 
+
+
 app = FastAPI()
+
+
+
 
 #Fonction qui permet de détecté les spam j'ai mis une liste de mot assez bateau
 def classify_message(message):
@@ -14,6 +22,13 @@ def classify_message(message):
         if word in message.lower():
             return "spam"
     return "ham"
+
+
+templates = Jinja2Templates(directory="templates")
+
+@app.get("/", response_class=HTMLResponse)
+def read_root(request: Request):
+    return templates.TemplateResponse("main.html", {"request": request})
 
 #Permet de tester les messages
 @app.get("/check")
